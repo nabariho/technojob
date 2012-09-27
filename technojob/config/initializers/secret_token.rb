df8795dc1c3e30af4ad295cdb0157c9cffa6bf81
nabariho <nabariho@gmail.com>
2012-09-27 19:22:54 +0100

    branch for company registration

---
+++ technojob/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Technojob::Application.config.secret_token = 'bc45b39b0f940b97f63b8721b51740f774113fdf82df01fcb0ae12e789012edecb76f8452d4ec31b261ea115d81a4fcf16d404e70a6766c655d0e7ecfe2d1ee2'
---
+++ technojob/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Technojob::Application.config.session_store :cookie_store, key: '_technojob_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Technojob::Application.config.session_store :active_record_store
